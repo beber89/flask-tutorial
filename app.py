bf76121e8cce67524781c06a628e45155f6651d4
unknown <mahmoudbaibars@gmail.com>
2021-04-16 00:43:35 +0200

    feat: add table and input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,8 +15,12 @@ class TODO(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        pass
+    else:
+        pass
     return render_template("index.html")
 
 if __name__ == "__main__":
